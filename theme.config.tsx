e9e0a6c4c6c4c58d3bdee9e03eeada97f40b8ac1
e.mercado <admin@wayalabs.com>
2023-04-23 23:20:28 -0400

    updates to navBar and correcting links

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>web3-4dummies</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/0xwaya/web3dapp-docs',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discordapp.com/users/0xwaya#6613',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/0xwaya/web3dapp-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '0xwaya Docs Template',
   },
 }
 
